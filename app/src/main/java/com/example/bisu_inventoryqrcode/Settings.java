7b53b731ef52317ffefd837f7b2324072f794ddc
JorgB123 <158415619+JorgB123@users.noreply.github.com>
2024-02-02 11:22:43 +0800

    Update Settings.java

--- app/src/main/java/com/example/bisu_inventoryqrcode/Settings.java
+++ app/src/main/java/com/example/bisu_inventoryqrcode/Settings.java
@@ -27,7 +27,7 @@ public class Settings extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
+        //edit
         
 
         ipAddressManager = new IPAddressManager(getApplicationContext());
